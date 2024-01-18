95cc0d4c4a98da011207d4a31c9ef2407e9d1f47
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2024-01-18 14:00:03 +0200

    fix fix fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from '../redux/store';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactsList/ContactList';
